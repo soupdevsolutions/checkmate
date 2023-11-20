1cf2e0c342f84fc5facbf293fe23ca5c5efb5263
mirch <mirceateodor.oprea@gmail.com>
2023-11-20 23:00:52 +0200

    Oops, complete the security group rules

--- infrastructure/security.tf
+++ infrastructure/security.tf
@@ -1,18 +1,27 @@
 resource "aws_security_group" "healthchecker_be" {
-    name = "healthchecker-be"
-    description = "Healthchecker backend security group"
-    vpc_id = aws_vpc.healthchecker.id
+  name        = "healthchecker-be"
+  description = "Healthchecker backend security group"
+  vpc_id      = aws_vpc.healthchecker.id
 
-    tags = {
+  tags = {
     Name = "healthchecker-be"
   }
 }
 
 resource "aws_security_group_rule" "healthchecker_be_inbound" {
-    security_group_id = aws_security_group.healthchecker_be.id
-    type = "ingress"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ""
+  security_group_id = aws_security_group.healthchecker_be.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "http"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "healthchecker_be_inbound" {
+  security_group_id = aws_security_group.healthchecker_be.id
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
