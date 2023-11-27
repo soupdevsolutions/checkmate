d17fa672c0729c86d213f9d83af9cc4322358c44
mirch <mirceateodor.oprea@gmail.com>
2023-11-27 12:09:13 +0200

    Add missing resources

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,16 +1,29 @@
 resource "aws_iam_role" "healthchecker-be" {
-    assume_role_policy = data.aws_iam_policy_document.healthchecker-be.json
+  assume_role_policy = data.aws_iam_policy_document.healthchecker-be.json
 }
 
 data "aws_iam_policy_document" "healthchecker-be" {
-    statement {
-        actions = [
-            "sts:AssumeRole",
-        ]
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
 
-        principals {
-            type        = "Service"
-            identifiers = ["ec2.amazonaws.com"]
-        }
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
     }
+  }
 }
+
+resource "aws_iam_role_policy" "healthchecker-be" {
+  name   = "healthchecker-be"
+  role   = aws_iam_role.healthchecker-be.id
+  policy = data.aws_iam_policy_document.healthchecker-be.json
+}
+
+resource "aws_iam_policy_attachment" "healthchecker-be" {
+  name       = "healthchecker-be"
+  roles      = [aws_iam_role.healthchecker-be.name]
+  policy_arn = aws_iam_role_policy.healthchecker-be.arn
+}
+
