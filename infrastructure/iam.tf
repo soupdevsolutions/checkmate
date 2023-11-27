c63e69f051d227f92e35c39a66827fdae41f949f
mirch <mirceateodor.oprea@gmail.com>
2023-11-27 14:17:40 +0200

    Reference the assume policy :)

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "healthchecker-be" {
-  assume_role_policy = data.aws_iam_policy_document.healthchecker-be.json
+  assume_role_policy = data.aws_iam_policy_document.healthchecker-be-assume-policy.json
 }
 
 data "aws_iam_policy_document" "healthchecker-be-assume-policy" {
