30811c30c0c5f1e3115d099411eb961f6802cb13
mirch <mirceateodor.oprea@gmail.com>
2023-11-28 13:16:58 +0200

    Push the docker repo separately

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.26.0"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
   }
 
   backend "s3" {
@@ -23,12 +19,4 @@ provider "aws" {
   region     = var.AWS_REGION
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
-provider "docker" {
-  registry_auth {
-      address = data.aws_ecr_authorization_token.token.proxy_endpoint
-      username = data.aws_ecr_authorization_token.token.user_name
-      password  = data.aws_ecr_authorization_token.token.password
-    }
 }
