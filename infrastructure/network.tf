08e4bcc16c517ae7b95806dca439671e03c84758
mirch <mirceateodor.oprea@gmail.com>
2023-11-29 22:54:28 +0200

    Add aws_lb_listener

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -30,3 +30,14 @@ resource "aws_lb_target_group" "healthchecker" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.healthchecker.id
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.healthchecker-be-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.healthchecker.arn
+  }
+}
